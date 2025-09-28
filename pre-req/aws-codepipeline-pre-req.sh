0005a04f0354f7cc2cf30745714175d29543342f
Nicolas <nayepesc@udistrital.edu.co>
2025-09-28 08:07:26 -0500

    Se creó el script de bash para desplegar los recursos de CloudFormation declarados, especificando la región, el script y las capabilities

---
+++ pre-req/aws-codepipeline-pre-req.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+aws cloudformation deploy \
+--region us-west-2\
+--stack-name codepipeline-lambda-prereq \
+--template-file ./aws-codepipeline-pre-req.yml \
+--capabilities CAPABILITY_IAM \
+--capabilities CAPABILITY_NAMED_IAM
