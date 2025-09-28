09495f773ec2df96cecd34d18cb36b996706e894
Nicolas <nayepesc@udistrital.edu.co>
2025-09-28 10:54:27 -0500

    fix: Se corrigió el error en el script, que hacía que no se reconociera el stack-name

--- pre-req/aws-codepipeline-pre-req.sh
+++ pre-req/aws-codepipeline-pre-req.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 aws cloudformation deploy \
---region us-east-2\
+--region us-east-2 \
 --stack-name codepipeline-lambda-prereq \
 --template-file ./aws-codepipeline-pre-req.yml \
 --capabilities CAPABILITY_IAM \
